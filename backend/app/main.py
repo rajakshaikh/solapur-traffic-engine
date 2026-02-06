28dc25fdfe57a8d4c640f791c365a05908d6511d
rajakshaikh <rajak9262shaikh@gmail.com>
2026-02-06 23:24:19 +0530

    Fix CORS for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
